031d47637f5f29396b622955ac9918c993444da4
vbalaji215-dev <vbalaji215@outlook.com>
2023-03-04 07:35:12 -0500

    ZEUS-210 updated to use uuid as key

--- src/main/java/com/brihaspathee/zeus/web/request/InternalRefDataRequest.java
+++ src/main/java/com/brihaspathee/zeus/web/request/InternalRefDataRequest.java
@@ -5,8 +5,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 /**
  * Created in Intellij IDEA
